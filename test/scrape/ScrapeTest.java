ffad8a922e67c72a0a62666026b0305fddcad95c
abdulajet <abdulhakimajet97@live.com>
2017-01-25 10:52:16 +0000

    hostname test

---
+++ test/scrape/ScrapeTest.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scrape;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author abdulhakim
+ */
+public class ScrapeTest {
+
+    public ScrapeTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Scrape.
+     */
+    @Test
+    public void testMain() {
+
+    }
+
+    /**
+     * Test of request method, of class Scrape.
+     */
+    @Test
+    public void testRequest() throws Exception {
+
+    }
+
+    /**
+     * Test of checkSubOrCrossDomain method, of class Scrape.
+     */
+    @Test
+    public void testCheckSubOrCrossDomain() throws Exception {
+        System.out.println("checkSubOrCrossDomain");
+        boolean subdomain = Scrape.checkSubOrCrossDomain("http://google.com", "http://mail.google.com");
+        boolean crossDomain = Scrape.checkSubOrCrossDomain("http://google.com", "http://youtube.com");
+        boolean sameDomain = Scrape.checkSubOrCrossDomain("http://google.com", "http://google.com/hello");
+
+        assertEquals(false, subdomain);
+        assertEquals(false, crossDomain);
+        assertEquals(true, sameDomain);
+    }
+
+}
